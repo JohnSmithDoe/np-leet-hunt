da5c80cbea03cb677af620f390114622a9a7c3fd
c0dem0nkey <you@example.com>
2023-10-07 11:53:25 +0200

    Leet-7: Nyan console :D

--- apps/np-leet-hunt/src/main.ts
+++ apps/np-leet-hunt/src/main.ts
@@ -1,13 +1,14 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { nyanConsole } from '../../../libs/np-library/src/lib/np-utils';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
     enableProdMode();
 }
-
+nyanConsole('Welcome to Leet-Hunt :)');
 platformBrowserDynamic()
     .bootstrapModule(AppModule)
     .catch(err => console.log(err));
---
+++ libs/np-library/README.md
@@ -0,0 +1,7 @@
+# np-library
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test np-library` to execute the unit tests.
